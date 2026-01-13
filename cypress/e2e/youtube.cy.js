f35235dedeb605ce06ead59e13aa688108c10ac5
om pokar <pokarom203@gmail.com>
2026-01-13 17:52:14 +0530

    first commit

---
+++ cypress/e2e/youtube.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+it('youtube search', function(){
+
+    cy.visit('https://www.youtube.com');
+
+    cy.get('[name="search_query"]').type("chai or code");
+
+    cy.get('.ytSearchboxComponentClearButton > .yt-spec-touch-feedback-shape > .yt-spec-touch-feedback-shape__fill').click();
+
+    cy.wait(2000);
+    
+    cy.get(':nth-child(3) > #endpoint > #icon > .yt-icon-shape > div').click();
+
+    cy.get(':nth-child(4) > #endpoint > #icon > .yt-icon-shape > div').click();
+
+
+
+})
